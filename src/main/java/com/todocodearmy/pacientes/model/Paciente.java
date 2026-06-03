bceb99c0a9b8a4d198b7e60c48e55193923e445c
arielibarbalz <ibarbalz@gmail.com>
2026-06-03 11:27:57 -0300

    Configuracion de application.properties en Windows

--- src/main/java/com/todocodearmy/pacientes/model/Paciente.java
+++ src/main/java/com/todocodearmy/pacientes/model/Paciente.java
@@ -2,6 +2,9 @@ package com.todocodearmy.pacientes.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +16,8 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPaciente;
     private String dni;
     private String nombre;
